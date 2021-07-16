6e2cdb50137e9688c9bd6c6d273d9de6e492084f
jeremiasmoncion <jeremiasmoncion@gmail.com>
2021-07-16 17:37:15 -0400

    cap 5 props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import Componente from './components/Componente';
+import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
 import './App.css';
 
 function App() {  
   return (   
     <div className="App">
       <header className="App-header">
+
+        <section>
         <img src={logo} className="App-logo" alt="logo" />       
         <p>
           Editar <code>src/App.js</code> and save to reload.
@@ -19,11 +22,28 @@ function App() {
         >
           Learn React
         </a>
+
+        
+        <section>
+
+      </section>
+          <Componente msg ="Primer Componenente 1"/>
+          <hr></hr>
+          <Propiedades 
+          cadena="Esto es una Cadena de Texto" 
+          numero={19} 
+          booleano = {true}
+          arreglo = {[1,2,3]}
+          objeto = {{nombre:"Juan", correo:"Juan@gmail.com"}}
+          function = {num => num * num}
+          elementoReact = {<i> Esto es un elemento React</i>}
+          componenteReact = {<Componente msg = "mensaje de componente desde propiedad"></Componente>}></Propiedades>
+        </section>
+
+      
       </header>
 
-      <section>
-        <Componente msg ="Primer Componenente 1"/>
-      </section>
+ 
 
     </div>    
 
